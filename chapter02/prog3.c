24dd67886a843d4e5def643c506f3332a651b76d
Eric Golovin <ericgolovin@Erics-MacBook-Pro.local>
2019-08-27 16:13:49 +0300

    added progs

---
+++ chapter02/prog3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (void) {
+	// Chapter 2, ex2
+	printf("In C, lowercase letters are significant.\nmain() is where program execution begins.\nOpening and closing braces enclose program statements in a routine.\nAll program statements must be terminated by a semicolon.");
+	return 0;
+}
+
