a00c07e2c5bee0c22a474e25750da59a37bfeac8
Eric Golovin <ericgolovin@outlook.com>
2019-10-02 09:08:40 +0300

    added exercises

--- chapter07/prog7-8.c
+++ chapter07/prog7-8.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-float squereRoot(float x, const float epsilon) {
-	float guess = 1.0;
-	float  absoluteValue(float x);
+double  squereRoot(double  x, const double  epsilon) {
+	double  guess = 1.0;
+	double   absoluteValue(double  x);
 	printf("\nguess = %f\n", guess);
 
 	if (x < 0) {
@@ -20,7 +20,7 @@ float squereRoot(float x, const float epsilon) {
 	return guess;
 }
 
-float absoluteValue(float x) {
+double  absoluteValue(double  x) {
 	if (x < 0) {
 		x = -x;
 	}
@@ -29,9 +29,9 @@ float absoluteValue(float x) {
 }
 
 int main(void) {
-	printf("squereRoot(2.0) = %f\n", squereRoot(2.0, .000001));
-	printf("squereRoot(144.0) = %f\n", squereRoot(144.0, .0001));
-	printf("squereRoot(17.5) = %f\n", squereRoot(17.5, .001));
+	printf("squereRoot(2.0) = %la\n", squereRoot(2.0, .000001));
+	printf("squereRoot(144.0) = %la\n", squereRoot(144.0, .0001));
+	printf("squereRoot(17.5) = %la\n", squereRoot(17.5, .001));
 
 	return 0;
 }
