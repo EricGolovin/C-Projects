5ada8f4eb7b26aa94c7137634ab966ccc45a86e0
Eric Golovin <ericgolovin@outlook.com>
2019-10-03 18:07:01 +0300

    added exercises

--- UniversityProjects/PR-Lab01/01-02/pr17.c
+++ UniversityProjects/PR-Lab01/01-02/pr17.c
@@ -1,16 +1,26 @@
-//
-// Created by Eric Golovin on 9/24/19.
-//
+#include <stdio.h>
+#include <math.h>
 
 int main(void) {
-    double x = 1;
-    double indexX = 0.5;
-    double a = 2;
-    double indexA = 1;
+	double x;
+	double indexX = 0.5;
+	double a;
+	double indexA = 1.0;
 
-    for (a = 2; a <= 10; x += indexX, a += indexA) {
-        printf("a2 + x = %lf", pow(a, 2) + x);
-    }
+	for (x = 1, a = 2; x <= 8.1; x += indexX) {
+		if (a <= 10.1) {
+			a += indexA;
+		}
 
-    return 0;
+		if (a > x) {
+			printf("%.2lf ^2 + %.2lf ==> %.2lf\n", a, x, pow(a, 2) + x);
+		} else if (a = x) {
+			printf("%.2lf ^2 ==> %.2lf\n", x, pow(a, 2));
+		}
+		else if (a < x) {
+			printf("%.2lf ^2 - %.2lf ==> %.2lf\n", a, x, pow(a, 2) - x);
+		}
+	}
+
+	return 0;
 }
