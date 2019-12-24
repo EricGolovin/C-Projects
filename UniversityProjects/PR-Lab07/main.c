642ca0741799bd85ce9de9dbe5ef9a697cf063fe
egolovin <ericgolovin@outlook.com>
2019-12-24 12:40:41 +0200

    regular commit

--- UniversityProjects/PR-Lab07/main.c
+++ UniversityProjects/PR-Lab07/main.c
@@ -6,6 +6,36 @@
 #include <stdlib.h>
 #include <string.h>
 
+void mx_printint(int n, int *file) {
+	int num = n;
+	int number;
+	int index = 0;
+
+	while (num > 0)
+        {
+		number = num % 10;
+		++index;
+		num /= 10;
+        }
+
+	num = n;
+	int array[index];
+	int i = 0;
+
+	while (num > 0)
+        {
+                number = num % 10;
+                array[i] = number;
+		i++;
+                num /= 10;
+        }
+
+	for (i = index - 1; i >= 0; --i) {
+		char numCh = array[i] + '0';
+		write(*file, &numCh, sizeof(numCh));
+	}
+}
+
 int main(int argc, char *argv[])
 {
     int openedFile = open(argv[1], O_RDWR);
@@ -13,6 +43,7 @@ int main(int argc, char *argv[])
     char stringNum[32];
     int index = 0;
 
+    int newFile = creat("outData.txt", O_RDWR);
     int columns = 0;
     int rows = 0;
 
@@ -20,7 +51,7 @@ int main(int argc, char *argv[])
 
     while (read(openedFile, &buffChar, 1))
     {
-        printf("%c", buffChar);
+	write(newFile, &buffChar, sizeof(buffChar));
         if (isdigit(buffChar))
         {
             stringNum[index] = buffChar;
@@ -53,7 +84,7 @@ int main(int argc, char *argv[])
             }
         }
     }
-    printf(" -> \n");
+    write (newFile, " -> \n", sizeof(" -> \n"));
 
     int newArray[rows][columns];
     int rowIndex = 0;
@@ -69,6 +100,15 @@ int main(int argc, char *argv[])
         if (isspace(buffChar) && stringNum[0] != '\0')
         {
             newArray[rowIndex][columnIndex] = atoi(stringNum);
+	    write(newFile, "newArray[", sizeof("newArray["));
+	    char rowIndexCh = rowIndex + '0';
+	    write(newFile, &rowIndexCh, sizeof(rowIndexCh));
+	    write(newFile, "][", sizeof("]["));
+	    char columnIndexCh = columnIndex + '0';
+	    write(newFile, &columnIndexCh, sizeof(columnIndexCh));
+	    write(newFile, "] = ", sizeof("] = "));
+	    write(newFile, stringNum, sizeof(stringNum));
+	    write(newFile, "\n", sizeof("\n"));
             printf("newArray[%i][%i] = %s\n", rowIndex, columnIndex, stringNum);
 
             while (index != 0)
@@ -90,8 +130,19 @@ int main(int argc, char *argv[])
         }
     }
 
+    write(newFile, "\nrows = ", sizeof("\nrows = "));
+    char rowsCh = rows + '0';
+    write(newFile, &rowsCh, sizeof(rowsCh));
+
+    write(newFile, "\ncolumns = ", sizeof("\ncolumns = "));
+    char columnsCh = columns + '0';
+    write(newFile, &columnsCh, sizeof(columnsCh));
+    write(newFile, "\n", sizeof("\n"));
+
     printf("\nrows = %i", rows);
     printf("\ncolumns = %i", columns);
+
+    write(newFile, "\nNew Array:\n", sizeof("\nNew Array:\n"));
 
     printf("\nNew Array:\n");
 
@@ -99,16 +150,25 @@ int main(int argc, char *argv[])
     {
         for (int j = 0; j < columns; j++)
         {
-            printf("%i ", newArray[i][j]);
+		char nArrayIndexCh;
+		if (newArray[i][j] < 0) {
+			nArrayIndexCh = -newArray[i][j] + '0';
+		} else {
+			nArrayIndexCh = newArray[i][j] + '0';
+		}
+		write(newFile, &nArrayIndexCh, sizeof(nArrayIndexCh));
+		write(newFile, " ", sizeof(" "));
+		printf("%i ", newArray[i][j]);
+
         }
+	write(newFile, "\n", sizeof("\n"));
         printf("\n");
     }
 
-    close(openedFile);
-
-    //int newFile = create("outData.txt", O_RDWR);
-
+    close(openedFile); 
+    
     // finding sum of columns with no negative elements
+    
     int nonNegColSum = 0;
     int exceptionalArray[columns];
 
@@ -139,6 +199,8 @@ int main(int argc, char *argv[])
         }
     }
 
+    // finding the sum of all diagonnals parallel to the secondary diagonal
+    
     int sumDiagonals[columns + 1];
     for (int i = 0; i < columns + 1; i++)
     {
@@ -163,7 +225,7 @@ int main(int argc, char *argv[])
                     }
                     else
                     {
-                        sumDiagonals[counter] += newArray[i][j];
+                    	sumDiagonals[counter] += newArray[i][j];
                     }
                     printf("sumDiagonal[%i] = %i\n", counter, newArray[i][j]);
                 }
@@ -185,12 +247,20 @@ int main(int argc, char *argv[])
     for (int i = 0; i < columns + 1; i++)
     {
         printf("sumDiagonals[%i] = %i\n", i, sumDiagonals[i]);
-
     }
+
+    // writing data to the file  
+
+    write(newFile, "diagonalMin = ", sizeof("diagonalMin = "));
+    mx_printint(diagonalMin, &newFile); 
+    write(newFile, "\n", 2);
+    write(newFile, "nonNegSum = ", sizeof("nonNegSum = "));
+    mx_printint(nonNegColSum, &newFile);
+    write(newFile, "\n", sizeof("\n"));
 
     printf("diagonalMin = %i\n", diagonalMin);
     printf("\nnonNegSum = %i\n", nonNegColSum);
-    printf("\nsecondaryDiagonalSum = %i\n", secondaryDiagonalSum);
+   
 
     return 0;
 }
