7d738293b1d339d1e524d9e9928de74533fe41b6
egolovin <ericgolovin@outlook.com>
2019-12-26 19:28:21 +0200

    regular commit

--- UniversityProjects/PR-Lab07/main.c
+++ UniversityProjects/PR-Lab07/main.c
@@ -3,36 +3,14 @@
 #include <stdbool.h>
 #include <stdlib.h> // atoi
 
-void mx_writeint(int n, FILE *file)
-{
-    int num = n;
-    int number;
-    int index = 0;
+void mx_printTextToFiles(FILE *fileName01, FILE *fileName02, char text[]) {
+    fprintf(fileName01, "%s", text);
+    fprintf(fileName02, "%s", text);
+}
 
-    while (num > 0)
-    {
-        number = num % 10;
-        ++index;
-        num /= 10;
-    }
-
-    num = n;
-    int array[index];
-    int i = 0;
-
-    while (num > 0)
-    {
-        number = num % 10;
-        array[i] = number;
-        i++;
-        num /= 10;
-    }
-
-    for (i = index - 1; i >= 0; --i)
-    {
-        char numCh = array[i] + '0';
-        fwrite(&numCh, 1, sizeof(numCh), file);
-    }
+void mx_printIntToFiles(FILE *fileName01, FILE *fileName02, int integer) {
+    fprintf(fileName01, "%i", integer);
+    fprintf(fileName02, "%i", integer);
 }
 
 int main(int argc, char *argv[])
@@ -48,10 +26,13 @@ int main(int argc, char *argv[])
     int columns = 0;
     int rows = 0;
 
+    // finding the number of rows and columns
+
     bool sizeSwitcher = true;
 
     while ((buffChar = fgetc(openedFile)) != EOF)
     {
+        printf("%c", buffChar);
         fwrite(&buffChar, 1, sizeof(buffChar), newFile);
         fwrite(&buffChar, 1, sizeof(buffChar), binFile);
         if (isdigit(buffChar))
@@ -86,8 +67,20 @@ int main(int argc, char *argv[])
             }
         }
     }
-    fwrite(" -> \n", 1, sizeof(" -> \n"), newFile);
-    fwrite(" -> \n", 1, sizeof(" -> \n"), binFile);
+    printf("(the first 2 digits in %s file)", argv[1]);
+    
+    fprintf(newFile, "(the first 2 digits in %s file)", argv[1]);
+    fprintf(binFile, "(the first 2 digits in %s file)", argv[1]);
+    mx_printTextToFiles(newFile, binFile, "\n\nrows = ");
+    mx_printIntToFiles(newFile, binFile, rows);
+    mx_printTextToFiles(newFile, binFile, "\ncolumns = ");
+    mx_printIntToFiles(newFile, binFile, columns);
+    mx_printTextToFiles(newFile, binFile, "\n\n");
+
+    printf("\n\nrows = %i", rows);
+    printf("\ncolumns = %i\n\n", columns);
+
+    // creating and printing our array of numbers
 
     int newArray[rows][columns];
     int rowIndex = 0;
@@ -102,24 +95,15 @@ int main(int argc, char *argv[])
         }
         if (isspace(buffChar) && stringNum[0] != '\0')
         {
-            newArray[rowIndex][columnIndex] = atoi(stringNum);
-            fwrite("newArray[", 1, sizeof("newArray["), newFile);
-            fwrite("newArray[", 1, sizeof("newArray["), binFile);
-            char rowIndexCh = rowIndex + '0';
-            fwrite(&rowIndexCh, 1, sizeof(rowIndexCh), newFile);
-            fwrite(&rowIndexCh, 1, sizeof(rowIndexCh), binFile);
-            fwrite("][", 1, sizeof("]["), newFile);
-            fwrite("][", 1, sizeof("]["), binFile);
-            char columnIndexCh = columnIndex + '0';
-            fwrite(&columnIndexCh, 1, sizeof(columnIndexCh), newFile);
-            fwrite(&columnIndexCh, 1, sizeof(columnIndexCh), binFile);
-            fwrite("] = ", 1, sizeof("] = "), newFile);
-            fwrite("] = ", 1, sizeof("] = "), binFile);
-            fwrite(stringNum, 1, sizeof(stringNum), newFile);
-            fwrite(stringNum, 1, sizeof(stringNum), binFile);
-            fwrite("\n", 1, sizeof("\n"), newFile);
-            fwrite("\n", 1, sizeof("\n"), binFile);
-            printf("newArray[%i][%i] = %s\n", rowIndex, columnIndex, stringNum);
+            newArray[rowIndex][columnIndex] = atoi(stringNum);            
+            mx_printTextToFiles(newFile, binFile, "-->\tnewArray[");
+            mx_printIntToFiles(newFile, binFile, rowIndex);
+            mx_printTextToFiles(newFile, binFile, "][");
+            mx_printIntToFiles(newFile, binFile, columnIndex);
+            mx_printTextToFiles(newFile, binFile, "] = ");
+            mx_printTextToFiles(newFile, binFile, stringNum);
+            mx_printTextToFiles(newFile, binFile, "\n");
+            printf("-->\tnewArray[%i][%i] = %s\n", rowIndex, columnIndex, stringNum);
 
             while (index != 0)
             {
@@ -139,26 +123,9 @@ int main(int argc, char *argv[])
             }
         }
     }
+    fclose(openedFile);
 
-    fwrite("\nrows = ", 1, sizeof("\nrows = "), newFile);
-    fwrite("\nrows = ", 1, sizeof("\nrows = "), binFile);
-    char rowsCh = rows + '0';
-    fwrite(&rowsCh, 1, sizeof(rowsCh), newFile);
-    fwrite(&rowsCh, 1, sizeof(rowsCh), binFile);
-
-    fwrite("\ncolumns = ", 1, sizeof("\ncolumns = "), newFile);
-    fwrite("\ncolumns = ", 1, sizeof("\ncolumns = "), binFile);
-    char columnsCh = columns + '0';
-    fwrite(&columnsCh, 1, sizeof(columnsCh), newFile);
-    fwrite(&columnsCh, 1, sizeof(columnsCh), binFile);
-    fwrite("\n", 1, sizeof("\n"), newFile);
-    fwrite("\n", 1, sizeof("\n"), binFile);
-
-    printf("\nrows = %i", rows);
-    printf("\ncolumns = %i", columns);
-
-    fwrite("\nNew Array:\n", 1, sizeof("\nNew Array:\n"), newFile);
-    fwrite("\nNew Array:\n", 1, sizeof("\nNew Array:\n"), binFile);
+    mx_printTextToFiles(newFile, binFile, "\n\nNew Array:\n");
 
     printf("\nNew Array:\n");
 
@@ -166,28 +133,13 @@ int main(int argc, char *argv[])
     {
         for (int j = 0; j < columns; j++)
         {
-            char nArrayIndexCh;
-            if (newArray[i][j] < 0)
-            {
-                nArrayIndexCh = -newArray[i][j] + '0';
-            }
-            else
-            {
-                nArrayIndexCh = newArray[i][j] + '0';
-            }
-            fwrite(&nArrayIndexCh, 1, sizeof(nArrayIndexCh), newFile);
-            fwrite(&nArrayIndexCh, 1, sizeof(nArrayIndexCh), binFile);
-            fwrite(" ", 1, sizeof(" "), newFile);
-            fwrite(" ", 1, sizeof(" "), binFile);
+            mx_printIntToFiles(newFile, binFile, newArray[i][j]);
+            mx_printTextToFiles(newFile, binFile, " ");
             printf("%i ", newArray[i][j]);
         }
-        fwrite("\n", 1, sizeof("\n"), newFile);
-        fwrite("\n", 1, sizeof("\n"), binFile);
-
+        mx_printTextToFiles(newFile, binFile, "\n");
         printf("\n");
     }
-
-    fclose(openedFile);
 
     // finding sum of columns with no negative elements
 
@@ -242,7 +194,6 @@ int main(int argc, char *argv[])
                 }
                 if ((i + j + index) == (columns - 1))
                 {
-                    printf("addedSecon = %i newArray[%i][%i]\n", newArray[i][j], i, j);
                     if (newArray[i][j] < 0)
                     {
                         sumDiagonals[counter] += -newArray[i][j];
@@ -251,7 +202,6 @@ int main(int argc, char *argv[])
                     {
                         sumDiagonals[counter] += newArray[i][j];
                     }
-                    printf("sumDiagonal[%i] = %i\n", counter, newArray[i][j]);
                 }
             }
         }
@@ -263,31 +213,20 @@ int main(int argc, char *argv[])
     {
         if (diagonalMin < sumDiagonals[i])
         {
-            printf("diagonalMin = %i sumDiagonal[%i] = %i\n", diagonalMin, i, sumDiagonals[i]);
             diagonalMin = sumDiagonals[i];
         }
     }
 
-    for (int i = 0; i < columns + 1; i++)
-    {
-        printf("sumDiagonals[%i] = %i\n", i, sumDiagonals[i]);
-    }
+    // printing the results to the files and console
 
-    // writing answers to the txt and bin file
+    mx_printTextToFiles(newFile, binFile, "\ndiagonalMin = ");
+    mx_printIntToFiles(newFile, binFile, diagonalMin);
 
-    fwrite("diagonalMin = ", 1, sizeof("diagonalMin = "), newFile);
-    fwrite("diagonalMin = ", 1, sizeof("diagonalMin = "), binFile);
-    mx_writeint(diagonalMin, newFile);
-    mx_writeint(diagonalMin, binFile);
-    fwrite("\nnonNegSum = ", 1, sizeof("\nnonNegSum = "), newFile);
-    fwrite("\nnonNegSum = ", 1, sizeof("\nnonNegSum = "), binFile);
-    mx_writeint(diagonalMin, newFile);
-    mx_writeint(diagonalMin, binFile);
-    fwrite("\n", 1, sizeof("\n"), newFile);
-    fwrite("\n", 1, sizeof("\n"), binFile);
+    mx_printTextToFiles(newFile, binFile, "\nnonNegSum = ");
+    mx_printIntToFiles(newFile, binFile, nonNegColSum);
 
     printf("diagonalMin = %i\n", diagonalMin);
-    printf("\nnonNegSum = %i\n", nonNegColSum);
+    printf("nonNegSum = %i\n", nonNegColSum);
 
     fclose(newFile);
     fclose(binFile);
