166041879ef58400abd6d10b55caa05b1694fad4
EricGolovin <ericgolovin@outlook.com>
2020-03-23 17:24:39 +0200

    sorted pr folder

---
+++ UniversityProjects/PR/02semester/PR-lab01/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+unsing namespace std;
+
+struct AEROFLOT {
+    char *destinationName;
+    int flightNumber;
+    char *planeModel;
+
+}
+
+int main(int argc, char *argv[]) {
+    
+    return 0;
+}
