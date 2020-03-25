a68fa9c3dc2decc43f2353200bf65ad048bbea72
EricGolovin <ericgolovin@outlook.com>
2020-03-25 22:21:26 +0200

    added

--- UniversityProjects/DM-semester02-lab01/main.cpp
+++ UniversityProjects/DM-semester02-lab01/main.cpp
@@ -6,19 +6,6 @@
 //  Copyright © 2020 Eric Golovin. All rights reserved.
 //
 //
-//#include "main.h"
-//
-////1,1;1,2;1,3;1,5;2,1;2,3;2,4;2,9;3,1;3,3;3,4;3,6;4,1;4,2;4,4;5,1;5,3;6,1;6,2;6,3;6,4;6,6;7,3;7,6;9,1;9,2;9,5;9,9
-//
-//int main(int argc, char* argv[]) {
-//    
-//    printAdjacencyMatrix();
-//    cout << endl;
-//    printIncidenceMatrix();
-//    
-//    return 0;
-//}
-
 #include <iostream>
 using namespace std;
 
