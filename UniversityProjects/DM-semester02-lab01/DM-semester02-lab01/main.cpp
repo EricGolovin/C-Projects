9242bd1f45dd2d3673ece1da80a6e94e994e3d53
EricGolovin <ericgolovin@outlook.com>
2020-03-25 22:06:28 +0200

    share

--- UniversityProjects/DM-semester02-lab01/DM-semester02-lab01/main.cpp
+++ UniversityProjects/DM-semester02-lab01/DM-semester02-lab01/main.cpp
@@ -27,16 +27,16 @@ struct Graph {
     int y;
 };
 
-void adjacency(int adjacencyMatrix[11][11], int reachabilityMatrix[11][11]) {
-    for (int i = 0; i < 11; ++i) {
-        for (int j = 0; j < 11; ++j) {
+void adjacency(int adjacencyMatrix[9][9], int reachabilityMatrix[9][9]) {
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) {
             reachabilityMatrix[i][j]=adjacencyMatrix[i][j];
         }
     }
     
-    for (int k = 0; k < 11; k++) {
-        for (int i = 0; i < 11; i++) {
-            for (int j = 0; j < 11 ; j++) {
+    for (int k = 0; k < 9; k++) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9 ; j++) {
                 reachabilityMatrix[i][j]=(reachabilityMatrix[i][j]||(reachabilityMatrix[k][j]&&reachabilityMatrix[i][k]));
             }
         }
@@ -48,9 +48,9 @@ int main() {
     
     Graph g[28] = {{1, 1}, {1, 2}, {1, 3}, {1, 5}, {2, 1}, {2, 3}, {2, 4}, {2, 9}, {3, 1}, {3, 3}, {3, 4}, {3, 6}, {4, 1}, {4, 2}, {4, 4}, {5, 1}, {5, 3}, {6, 1}, {6, 2}, {6, 3}, {6, 4}, {6, 6}, {7, 3}, {7, 6}, {9, 1}, {9, 2}, {9, 5}, {9, 9}};
     
-    int smezhn[11][11] = { 0 };
-    int intsed[11][35] = { 0 };
-    int dostezh[11][11] = { 0 };
+    int smezhn[9][9] = { 0 };
+    int intsed[9][28] = { 0 };
+    int dostezh[9][9] = { 0 };
     
     for (int i = 0; i < 28; ++i) {
         smezhn[g[i].x - 1][g[i].y - 1] = 1;
@@ -58,8 +58,8 @@ int main() {
     
     cout << "Матрица смежности:" << endl;
     
-    for (int i = 0; i < 11; ++i, cout << '\n') {
-        for (int j = 0; j < 11; ++j) {
+    for (int i = 0; i < 9; ++i, cout << '\n') {
+        for (int j = 0; j < 9; ++j) {
             cout.width(3);
             cout << smezhn[i][j] << ' ';
         }
@@ -78,7 +78,7 @@ int main() {
     
     cout << "\nМатрица инцедентности:\n";
     
-    for (int i = 0; i < 11; ++i, cout << '\n') {
+    for (int i = 0; i < 9; ++i, cout << '\n') {
         for (int j = 0; j < 28; ++j) {
             cout.width(2);
             cout << intsed[i][j];
@@ -90,8 +90,8 @@ int main() {
     
     adjacency(smezhn,dostezh);
     
-    for (int i = 0; i < 11; ++i, cout << '\n') {
-        for (int j = 0; j < 11; ++j) {
+    for (int i = 0; i < 9; ++i, cout << '\n') {
+        for (int j = 0; j < 9; ++j) {
             cout.width(3);
             cout << dostezh[i][j] << ' ';
         }
