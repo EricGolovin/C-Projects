dc6888fd8baa2862113f09c9a5720ea58dcefbc0
EricGolovin <ericgolovin@outlook.com>
2020-03-29 00:14:03 +0200

    completed fileContent to structs

--- UniversityProjects/PR/02semester/PR-lab01/main.cpp
+++ UniversityProjects/PR/02semester/PR-lab01/main.cpp
@@ -1,34 +1,95 @@
 #include <iostream>
-#include <vector>
+//#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+
 using namespace std;
 
 typedef struct AEROFLOT {
-    const char* destinationName;
+    string destinationName;
     int flightNumber;
-    const char *planeModel;
+    string planeModel;
 } AEROFLOT;
 
-void printArrayOfStructs(vector<AEROFLOT> str) {
+//void printArrayOfStructs(vector<AEROFLOT> str) {
+//    cout << endl;
+//    for (int i = 0; i < str.size(); i++) {
+//        cout << "struct #" << i + 1 << endl;
+//        cout << "Destination Name" << str[i].destinationName << endl;
+//        cout << "Flight Number" << str[i].flightNumber << endl;
+//        cout << "Plane Model" << str[i].planeModel << endl << endl;
+//    }
+//}
+
+void printArrayOfStructs(AEROFLOT str[], int num) {
     cout << endl;
-    for (int i = 0; i < str.size(); i++) {
+    for (int i = 0; i < num; i++) {
         cout << "struct #" << i + 1 << endl;
         cout << "Destination Name" << str[i].destinationName << endl;
-        cout << "Flight Number" << str[i].flightNumber << endl;
+        cout << "Flight Number " << str[i].flightNumber << endl;
         cout << "Plane Model" << str[i].planeModel << endl << endl;
     }
 }
 
 int main(int argc, char *argv[]) {
-    AEROFLOT flight = {"Denmark", 342, "Boing"};
+    fstream outFile;
+    stringstream stringStream;
+    string fileName;
+    //    vector<AEROFLOT> dynArrayOfAeroflots (0);
     
-    vector<AEROFLOT> dynArrayOfAeroflots (0);
+    int totalFlights = 0;
     
-    for (int i = 0; i < 5; i++) {
-        AEROFLOT someFlight = {"Santa Clara", 277, "BoingHQR"};
-        dynArrayOfAeroflots.push_back(someFlight);
+    outFile.open("testFile.txt", ios_base::in);
+    
+    if (outFile.is_open()) {
+        cout << "File open successful. It contains: " << endl;
+        string fileContent;
+        
+        while (outFile.good()) {
+            getline(outFile, fileContent);
+            stringStream << totalFlights << "-" << fileContent << endl;
+            totalFlights++;
+        }
+        
+        outFile.close();
     }
     
-    printArrayOfStructs(dynArrayOfAeroflots);
+    string fileData = stringStream.str();
+    AEROFLOT arrayOfFlights[totalFlights];
+    
+    for (int i = 0; i < totalFlights; i++) {
+        stringstream stringNumStream;
+        AEROFLOT someFlight;
+        string destName;
+        int flightNum;
+        string planeModl;
+        
+        for (int index = 0, flag = 0, parameterNumber = 0; index < fileData.length() - 1; index++) {
+            if (fileData[index] == '{') {
+                flag = 1;
+                parameterNumber++;
+                continue;
+            } else if (fileData[index] == '}') {
+                flag = 0;
+                continue;
+            }
+            if (flag) {
+                if (parameterNumber == 1) {
+                    someFlight.destinationName += fileData[index];
+                } else if (parameterNumber == 2) {
+                    stringNumStream << fileData[index];
+                } else if (parameterNumber == 3) {
+                    stringNumStream >> someFlight.flightNumber;
+                    someFlight.planeModel += fileData[index];
+                }
+            }
+        }
+        cout << someFlight.destinationName << ", " << someFlight.flightNumber << ", " << someFlight.planeModel << endl;
+        arrayOfFlights[i] = someFlight;
+    }
+    
+    printArrayOfStructs(arrayOfFlights, totalFlights);
     
     return 0;
 }
