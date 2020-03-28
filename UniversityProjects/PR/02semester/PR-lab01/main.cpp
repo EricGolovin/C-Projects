9ea7a3ee7995e55ca8bb9c28c33634ee289c2c0f
EricGolovin <ericgolovin@outlook.com>
2020-03-28 18:53:51 +0200

    added vector to PR lab

--- UniversityProjects/PR/02semester/PR-lab01/main.cpp
+++ UniversityProjects/PR/02semester/PR-lab01/main.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
-unsing namespace std;
+#include <vector>
+using namespace std;
 
-struct AEROFLOT {
-    char *destinationName;
+typedef struct AEROFLOT {
+    const char* destinationName;
     int flightNumber;
-    char *planeModel;
+    const char *planeModel;
+} AEROFLOT;
 
+void printArrayOfStructs(vector<AEROFLOT> str) {
+    cout << endl;
+    for (int i = 0; i < str.size(); i++) {
+        cout << "struct #" << i + 1 << endl;
+        cout << "Destination Name" << str[i].destinationName << endl;
+        cout << "Flight Number" << str[i].flightNumber << endl;
+        cout << "Plane Model" << str[i].planeModel << endl << endl;
+    }
 }
 
 int main(int argc, char *argv[]) {
+    AEROFLOT flight = {"Denmark", 342, "Boing"};
+    
+    vector<AEROFLOT> dynArrayOfAeroflots (0);
+    
+    for (int i = 0; i < 5; i++) {
+        AEROFLOT someFlight = {"Santa Clara", 277, "BoingHQR"};
+        dynArrayOfAeroflots.push_back(someFlight);
+    }
+    
+    printArrayOfStructs(dynArrayOfAeroflots);
     
     return 0;
 }
