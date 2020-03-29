2bc2749b288ed89ccb1d6d173a1892889d3a259c
EricGolovin <ericgolovin@outlook.com>
2020-03-29 15:41:12 +0300

    completed prog

--- UniversityProjects/PR/02semester/PR-lab01/main.cpp
+++ UniversityProjects/PR/02semester/PR-lab01/main.cpp
@@ -40,7 +40,7 @@ string readFileData(string fileName, int *numOfFlights) {
     outFile.open(fileName, ios_base::in);
     
     if (outFile.is_open()) {
-        cout << "File open successful. It contains: " << endl;
+        cout << "File opened successfuly." << endl << endl;
         string fileContent;
         
         while (outFile.good()) {
@@ -55,6 +55,44 @@ string readFileData(string fileName, int *numOfFlights) {
     return stringStream.str();
 }
 
+string findByPlaneModel(string planeToFind, AEROFLOT inArray[], int numOfElements) {
+    stringstream outputStringStream;
+    for (int i = 0; i < numOfElements; i++) {
+        if (planeToFind == inArray[i].planeModel) {
+            outputStringStream << "--------------------------------" << endl;
+            outputStringStream << "Index in the list: " << i + 1 << endl;
+            outputStringStream << "Detination Name: " << inArray[i].destinationName << endl;
+            outputStringStream << "Flight number: " << inArray[i].flightNumber << endl;
+            outputStringStream << "--------------------------------" << endl;
+        }
+    }
+    
+    if (outputStringStream.str() == "") {
+        return "--------------------------------\nThis flight does not exist.\n--------------------------------\n";
+    } else {
+        return outputStringStream.str();
+    }
+} 
+
+string findByCityName(string nameToFind, AEROFLOT inArray[], int numOfElements) {
+    stringstream outputStringStream;
+    for (int i = 0; i < numOfElements; i++) {
+        if (inArray[i].destinationName.find(nameToFind) != string::npos) {
+            outputStringStream << "--------------------------------" << endl;
+            outputStringStream << "Index in the list: " << i + 1 << endl;
+            outputStringStream << "Flight number: " << inArray[i].flightNumber << endl;
+            outputStringStream << "Plane Model: " << inArray[i].planeModel << endl;
+            outputStringStream << "--------------------------------" << endl;
+        }
+    }
+    
+    if (outputStringStream.str() == "") {
+        return "--------------------------------\nThis flight does not exist.\n--------------------------------\n";
+    } else {
+        return outputStringStream.str();
+    }
+}
+
 bool stringSorting(AEROFLOT flight01, AEROFLOT flight02) {
     return flight01.destinationName < flight02.destinationName;
 }
@@ -62,7 +100,7 @@ bool stringSorting(AEROFLOT flight01, AEROFLOT flight02) {
 int main(int argc, char *argv[]) {
     //    vector<AEROFLOT> dynArrayOfAeroflots (0);
     int totalFlights = 0;
-    string fileData = readFileData("testFile.txt", &totalFlights);
+    string fileData = readFileData("inputTestFile.txt", &totalFlights);
     AEROFLOT arrayOfFlights[totalFlights];
     
     for (int i = 0; i < totalFlights; i++) {
@@ -71,7 +109,7 @@ int main(int argc, char *argv[]) {
         string destName;
         int flightNum;
         string planeModl;
-
+        
         for (int index = 0, flag = 0, parameterNumber = 0; index < fileData.length() - 1; index++) {
             if (fileData[index] == '{') {
                 flag = 1;
@@ -92,15 +130,54 @@ int main(int argc, char *argv[]) {
                 }
             }
         }
-        cout << someFlight.destinationName << ", " << someFlight.flightNumber << ", " << someFlight.planeModel << endl;
+        // cout << someFlight.destinationName << ", " << someFlight.flightNumber << ", " << someFlight.planeModel << endl;
         arrayOfFlights[i] = someFlight;
         fileData.erase(fileData.find('D'), fileData.find('\n'));
-        cout << fileData << endl;
     }
     
-    printArrayOfStructs(arrayOfFlights, totalFlights);
     sort(arrayOfFlights, arrayOfFlights + totalFlights, stringSorting);
-    printArrayOfStructs(arrayOfFlights, totalFlights);
-        
+    
+    string typeOfSearch;
+    string searchParameter;
+    ofstream outputFile("outputFile.txt");
+    
+    cout << "Search by Plane Model or by Destination city Name (type <plane> or <city> down below): " << endl;
+    cin >> typeOfSearch;
+    
+    while (1) {
+        if (typeOfSearch == "plane" || typeOfSearch == "<plane>") {
+            cout << "Enter plane model: ";
+            cin >> searchParameter;
+            cout << endl;
+            
+            outputFile << findByPlaneModel(searchParameter, arrayOfFlights, totalFlights);
+            cout << findByPlaneModel(searchParameter, arrayOfFlights, totalFlights) << endl;
+            
+        } else if (typeOfSearch == "city" || typeOfSearch == "<city>") {
+            cout << "Enter Destination city Name: ";
+            cin >> searchParameter;
+            cout << endl;
+            
+            outputFile << findByCityName(searchParameter, arrayOfFlights, totalFlights);
+            cout << findByCityName(searchParameter, arrayOfFlights, totalFlights) << endl;
+        } else {
+            cout << "You have Entered nonexistent search Option." << endl;
+            cout << "Please, Enter <plane> or <city> down below: " << endl;
+            typeOfSearch = "";
+            cin >> typeOfSearch;
+            
+        }
+        string exitOption;
+        cout << endl << "type <continue> to Continue running of the search" << endl << "or" << endl << "Type <stop> to Stop running of the search" << endl;
+        cin >> exitOption;
+        cout << endl;
+
+        if (exitOption == "stop" || exitOption == "<stop>") {
+            break;
+        }
+    }
+    
+    outputFile.close();
+    
     return 0;
 }
